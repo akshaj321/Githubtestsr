251e723e91080f2ba1c50502865774934d2a0692
Akshaj Prabhakaran <akshaj.prabhakaran@sji.edu.sg>
2023-05-31 11:10:49 +0800

    Initial Commit

---
+++ Githubtestsr/GithubtestsrApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubtestsrApp.swift
+//  Githubtestsr
+//
+//  Created by Shakthi Vel on 31/5/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubtestsrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
